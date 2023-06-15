f14988af9150e601f7468a73a956e2faeaca3ab4
Natalia Bilenko <nbilenko@gmail.com>
2023-06-15 15:33:29 -0700

    gtag

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,25 @@
 import "../styles/globals.css";
+import Script from "next/script";
 import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider defaultTheme="light" attribute="class">
-      <Component {...pageProps} />
+      <Component {...pageProps}>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-N67H49L9YG"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-N67H49L9YG');
+        `}
+        </Script>
+      </Component>
     </ThemeProvider>
   );
 }
